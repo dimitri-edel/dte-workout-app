af3c7d827e3a00efae9192a7220c46963019caa4
Dimitri Edel <dimitri.edel@gmx.net>
2023-09-04 09:57:49 +0200

    Define Model Workload

--- workload/models.py
+++ workload/models.py
@@ -1,3 +1,23 @@
+""" Model for workload performed during each set of an exercise"""
+#pylint: disable=no-name-in-module
 from django.db import models
+from django.contrib.auth.models import User
+from workout.models import WorkoutExercise
 
-# Create your models here.
+class Workload(models.Model):
+    """Workload logs results of each set of an exercise during a workout session"""
+    # Owner of the workload
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner_workload")
+    # The relationship to the owner object of type WorkoutExercise  
+    workout_exercise = models.ForeignKey(WorkoutExercise,\
+     on_delete=models.CASCADE, related_name="workout_exercise_exercise_set", default=0)
+    # Number of repetitions in this set
+    reps = models.IntegerField(blank=True, null=True, default="0")
+    # The weight that was used, if weight lifting is involved
+    weight = models.IntegerField(blank=True, null=True, default="0")
+    # The time it took to complete the set, if it is a cardio exercise
+    time = models.CharField(blank=True, null=True, default="00:00:00:0")
+    # The distance covered in the amount of time specified in the time field
+    distance = models.FloatField(blank=True, null=True, default="0")
+    def __str__(self):
+        return f"{self.workout_exercise.__str__()}"
